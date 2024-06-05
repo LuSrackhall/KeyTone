9704de0ae99f98c02bebec598957744a07d793a5
LuSrackhall <3647637206@qq.com>
2024-06-05 21:03:41 +0800

    chore(gohook): 取消了Kind = 3 即 KeyDown 情况的监听, 并删除了与其相关的注释<因为写的太啰嗦>。 发现新bug<见69行注释>

--- sdk/main.go
+++ sdk/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	hook "github.com/robotn/gohook"
 )
 
@@ -18,10 +16,11 @@ func keyEventListen() {
 	keycode_keycodeChan_map := make(map[uint16]chan hook.Event)
 
 	for ev := range evChan {
-		if ev.Kind == 3 || ev.Kind == 4 || ev.Kind == 5 {
+		// if ev.Kind == 3 || ev.Kind == 4 || ev.Kind == 5 {
+		if ev.Kind == 4 || ev.Kind == 5 {
 			/* Kind
 			 *
-			 * KeyDown = 3
+			 * KeyDown = 3  // 由于goHook的bug, KeyDown事件对象中, 无法判断实际的Keycode(即Keycode始终为0), 因此我们不使用这个事件。转而利用KeyHold事件代替此事件。
 			 * KeyHold = 4
 			 * KeyUp   = 5
 			 *
@@ -35,23 +34,13 @@ func keyEventListen() {
 			 */
 			// if ev.Kind == 3 {
 			// 	println("down")
-			// 	println(ev.Keycode) // 按下时, 由于goHook的bug, 故无法判断实际的keyCode, 因此我们不使用这个事件。
+			// 	println(ev.Keycode) // 按下时, 由于goHook的bug, 故无法判断实际的Keycode, 因此我们不使用这个事件。
 			// }
 			if _, exists := keycode_keycodeChan_map[ev.Keycode]; exists {
 				// fmt.Println("此时已经有了处理此按键发音的通道与其专用的goroutine, 因此无需进行任何创建操作, 只需要向其传递最新的事件信号即可")
 				keycode_keycodeChan_map[ev.Keycode] <- ev
 			} else {
-				/*
-				 * 你可能会注意到, 在第一次按键按下时, 这里打印了两遍。
-				 * 原因是, KeyDown=3时, Keycode的值始终为0, 而我们监听的有3这个情况, 也就是创建了一个并未实际使用的通道和goroutine
-				 * 之前也提到过, 这是goHook库的一个bug, 因此我们也是使用 KeyHold , 来代替了 KeyDown。
-				 * 这里保留KeyDown只是为了等待goHook库的修复, 多余的这个 Keycode为0的 通道 和 goroutine 对我们的程序性能几乎没有任何影响。
-				 * * 虽每次按键都会想 keycode 为0 的通道传递对应事件, 但此事件到达对应的 keycode为0的goroutine中后, 仍会因逻辑而被直接抛弃。
-				 * * 因为handleKeyEvent中, 只判断了 Kind = 4 和 Kind = 5的处理逻辑, 因此影响不大。
-				 * 当然, 如果认为这会对性能造成影响, 可以直接在父逻辑中取消对 Kind = 3 情况的处理。
-				 *
-				 */
-				fmt.Println("此时还没有处理此按键发音的通道与其专用的goroutine, 因此需进行相关的创建操作, 并在创建后向其传递最新的事件信号")
+				// fmt.Println("此时还没有处理此按键发音的通道与其专用的goroutine, 因此需进行相关的创建操作, 并在创建后向其传递最新的事件信号")
 				// 创建此按键的专属通道channel
 				keycode_keycodeChan_map[ev.Keycode] = make(chan hook.Event)
 				// 创建此按键专属 按键事件处理 的 goroutine
@@ -77,6 +66,7 @@ func handleKeyEvent(evChan chan hook.Event) {
 			}
 			println(ev.Keycode) // 按下时, 由于goHook的bug, 无法判断实际的keyCode。 但由于hold的触发实际与down几乎一致, 且可判断实际的keyCode, 因此可使用此事件代替down
 		}
+		// 由于同时(或者说几乎同时)按下两个按键后, 只有最后一个被按下的按键会一直触发hold事件(等于说会造成先按下的按键可能无法借助此处判断来播放key_down的声音)
 		if (ev.When.UnixMilli()-time) > 300 && ev.Kind == 4 {
 			if !key_down_soundIsRun {
 				println("仅播放 key_down 声音")
