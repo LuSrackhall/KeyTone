a46ee15dc24022aa6891178b57c56e7385c0bb1c
LuSrackhall <3647637206@qq.com>
2024-06-18 22:34:45 +0800

    feat(frontend): 主界面暂时仅使用图标填充即可, 最初套壳版本无需任何多余功能。

--- frontend/src/pages/Main_page.vue
+++ frontend/src/pages/Main_page.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>Silent Typist's Friend</div>
+  <!-- <div>Silent Typist's Friend</div> -->
+  <q-page>
+    <div :class="['', 'w-auto h-60 flex flex-col items-center']">
+      <q-avatar
+        :class="[
+          '',
+          'm-t-10',
+          // 设置整体头像的尺寸大小。 至于内部相关文字的尺寸大小(如果有的话  可通过设置text-[8rem] leading-[10rem]实现)
+          'size-56',
+          '',
+        ]"
+      >
+        <img :src="logoUrl" />
+      </q-avatar>
+    </div>
+  </q-page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import logoUrl from 'assets/img/KeyTone.png?url';
+</script>
 
 <style lang="scss" scoped></style>
