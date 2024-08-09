2ac93a653c27010b05ca074be13723a4ac7c6599
LuSrackhall <3647637206@qq.com>
2024-08-09 20:27:50 +0800

    build(sdk): 更新了config模块, 使模块能够基于现有配置文件增量加载默认配置, 以应对未来版本更新时添加新配置的需求。
    commit #9

--- sdk/config/config.go
+++ sdk/config/config.go
@@ -21,15 +21,20 @@ func ConfigRun(path string) {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
 			// 配置文件未找到，创建默认设置的配置文件
 			logger.Info("未找到默认的配置文件，正在创建...")
-			createDefaultSetting()
+			createDefaultConfig()
+			logger.Info("配置文件创建并载入成功")
 		} else {
 			// 其他错误
 			logger.Error("读取设置文件时发生致命错误", "err", err.Error())
 		}
+	} else {
+		logger.Info("配置文件已加载, 正在与DefaultConfig进行diff和增量载入...")
+		diffAndUpdateDefaultConfig()
+		logger.Info("配置文件diff和增量载入完成")
 	}
 }
 
-// 恢复默认设置
+// 恢复默认设置(删除掉已有配置文件后, 再调用“createDefaultConfig()"函数就好了)
 func RestoreDefaultSetting() {
 
 }
@@ -37,7 +42,6 @@ func RestoreDefaultSetting() {
 // 获取配置
 // * 如果想获取这个json文件的所有配置, 则可用key="get_all_value"作为键值来查询
 func GetValue(key string) any {
-	// value := viper.GetString(key)
 	if key == "get_all_value" {
 		return viper.AllSettings()
 	} else {
@@ -53,57 +57,28 @@ func SaveNewValue(key string, value any) {
 	}
 }
 
-// 创建默认设置的配置文件
-func createDefaultSetting() {
+// 设置默认的配置
+func settingDefaultConfig() {
 	// 自动启动应用时的默认设置
-	// 注意: 第二个参数不能是结构体, 而应是一个map[string]interface{}。
-	// * 虽然interface{}可以表示map[string]interface{}, 但在go语言中, 直接使用interface{}来定义数据是编译器不可知不合法的。
-	// viper.Set("autoStartup", struct {
-	// 	isAutoRun     bool
-	// 	isHideWindows bool
-	// }{
-	// 	isAutoRun:     false,
-	// 	isHideWindows: true,
-	// })
-	// 第二个参数应该是一个map, 而不是一个结构体, 在go中我们使用map来映射json结构。
-	// * 另外, 直接interface{} 定义键值对，这是不允许的, 会导致编译错误,因为 interface{} 本身不是一个可索引的类型。
-	// * 不过, 反过来是可以使用 interface{}类型的变量, 来承载map[string]interface{}的, 只需在使用时进行"适当"的类型断言。
-	//   * {
-	//      	// 注意"适当", 当你不关注它时<如仅作为过程中的参数往后传递时>, 则无需断言它, 直接使用即可, 如案例中的变量aaa
-	//      	var aaa any
-	//      	bbb := map[string]interface{}{
-	//      		"isAutoRun":     false,
-	//      		"isHideWindows": true,
-	//      	}
-	//      	aaa = bbb
-	//      	viper.Set("autoStartup", aaa)
-	//
-	//        // 因此, 下方内容是合法的
-	//        {
-	//     	   	 // 定义一个空的 map，用于存放解析后的数据, 是合法的
-	//           // * 如果你不确定最终的json字符串的顶层结构是否对象(即映射)时
-	//           var value any
-	//           // 解析 JSON 字符串
-	//           err = json.Unmarshal([]byte(store_setting.Value), &value)
-	//        }
-	//     }
-	viper.Set("auto_startup", map[string]interface{}{
-		"is_auto_run":         false,
-		"is_hide_windows":     true,
-		"is_hide_windows_old": true,
-	})
+	viper.SetDefault("auto_startup.is_auto_run", false)
+	viper.SetDefault("auto_startup.is_hide_windows", true)
+	viper.SetDefault("auto_startup.is_hide_windows_old", true)
 
 	// 手动打开应用时的默认设置
-	viper.Set("startup", map[string]interface{}{
-		"is_hide_windows": false,
-	})
+	viper.SetDefault("startup.is_hide_windows", false)
+}
 
+func createDefaultConfig() {
+	settingDefaultConfig()
 	if err := viper.SafeWriteConfig(); err != nil {
-		logger.Error("创建默认设置文件时发生致命错误", "err", err.Error())
+		logger.Error("创建默认配置文件时发生致命错误", "err", err.Error())
 	}
 }
 
-// 检查默认设置的键名是否都存在, 不存在则仅针对不存在的key, 创建对应默认配置(即不改变用户已有的自定义过的配置的前提下, 将新增的默认配置内容加入配置文件)。
-func CheckDefaultSetting() {
-
+// 将默认配置增量写入配置文件<不会影响配置文件中已有的配置>
+func diffAndUpdateDefaultConfig() {
+	settingDefaultConfig()
+	if err := viper.WriteConfig(); err != nil {
+		logger.Error("diff并增量更新默认配置至现有配置文件时发生致命错误", "err", err.Error())
+	}
 }
