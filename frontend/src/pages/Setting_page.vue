d060ad67e6415a8adc09d93b1ee3e75a9c2af33a
LuSrackhall <3647637206@qq.com>
2024-08-12 23:27:43 +0800

    fix(ui): 修复了界面溢出问题, 现在可能超出的内容和可能产生的滚动条, 不再会溢出界面。
    commit #12

--- frontend/src/pages/Setting_page.vue
+++ frontend/src/pages/Setting_page.vue
@@ -1,30 +1,24 @@
 <template>
   <q-page>
-    <q-list>
-      <template v-for="(menuItem, index) in menuList" :key="index">
-        <q-expansion-item
-          default-opened
-          :icon="menuItem.icon"
-          :label="$t('setting.' + menuItem.label)"
-          :caption="$t('setting.' + menuItem.caption)"
-          :to="menuItem.to"
-          :header-inset-level="0"
-          :content-inset-level="0"
-        >
-          <language v-if="menuItem.to === '/setting-language'"></language>
-          <StartupAndAutoStartup v-if="menuItem.to === '/setting-startupAndAutoStartup'"></StartupAndAutoStartup>
-        </q-expansion-item>
-        <q-separator :key="'sep' + index" v-if="menuItem.separator" />
-      </template>
-    </q-list>
-    <!-- WARN:
-    // 验证当前页面滚动条溢出界面的bug -->
-    <!-- <div class="h-20"></div>
-    <div class="h-20"></div>
-    <div class="h-20"></div>
-    <div class="h-20"></div>
-    <div class="h-20"></div>
-    <div class="h-20"></div> -->
+    <q-scroll-area class="w-[379px] h-[458.5px]">
+      <q-list>
+        <template v-for="(menuItem, index) in menuList" :key="index">
+          <q-expansion-item
+            default-opened
+            :icon="menuItem.icon"
+            :label="$t('setting.' + menuItem.label)"
+            :caption="$t('setting.' + menuItem.caption)"
+            :to="menuItem.to"
+            :header-inset-level="0"
+            :content-inset-level="0"
+          >
+            <language v-if="menuItem.to === '/setting-language'"></language>
+            <StartupAndAutoStartup v-if="menuItem.to === '/setting-startupAndAutoStartup'"></StartupAndAutoStartup>
+          </q-expansion-item>
+          <q-separator :key="'sep' + index" v-if="menuItem.separator" />
+        </template>
+      </q-list>
+    </q-scroll-area>
   </q-page>
 </template>
 
