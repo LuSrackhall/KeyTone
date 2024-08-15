1a0a88f96e19993256dbc1b75a0b19e0bd8c9bdb
LuSrackhall <3647637206@qq.com>
2024-08-16 02:54:14 +0800

    fix: 修复了当音量提升/缩减滑块选择为小于-5或者说-g时, 主界面音量条件滑块异常的bug。<甚至仅小于0就有滑块范围缩小隐患, 原因是主页min音量的设计问题>
    commit #15

--- frontend/src/pages/Main_page.vue
+++ frontend/src/pages/Main_page.vue
@@ -31,6 +31,18 @@
               > > 当我们通过设置界面的音量强来增大原始音频音量时, 其音量的原始正常值将不再为0, 而是变为对音量增强的指数取相同基数后的对数值。
               > > * 即实际上音频的原始音量对应的数值是 `-setting_store.audioVolumeProcessing.volumeAmplify`
               > > 因此, 此时我们的最小值, 要想保持正常音量的对数 g , 需要 使用`-setting_store.audioVolumeProcessing.volumeAmplify-5`这个数值
+              > >   - completed(已完成)   FIXME:
+              > > * 当setting_store.audioVolumeProcessing.volumeAmplify > 0 时, 当然没问题不用过分讨论。
+              > > * 当setting_store.audioVolumeProcessing.volumeAmplify < 0 且  > -5 时, 使用这个算法就发生了问题(或者说永远保持正常音量的对数g的方案发生了问题)
+              > >   * 此时的缩小是仍旧在以缩小的基础上进行的(即此时的0), 而此时仍保证真实正常音量的对数g的话, 是不现实的, 因为主页的音量调整是一个只能缩小音量的滑块, 这要保持只能缩短滑块范围。
+              > >   * 拿-2来举例子, 貌似得出的最小值-3是更靠近正常音量了, 但实际上得出的这个最小值在处理时, 并不是一个更靠近正常音量的值(因为 -2 这个值, 在Amplify时已经被实际处理过了, 而这里在添一个-3, 仍旧是-5)
+              > >     * 或者说, 此时的0对应的已经是缩小后的音量了(而不是像正数时放大的音量), 我们无法通过只能缩减音量的滑块获得正常的音量, 此时要想进一步保持对数5, 就只能缩短滑块进度条的实际范围了。
+              > > * 当setting_store.audioVolumeProcessing.volumeAmplify < -5时, 问题就更大了。
+              > >   * 由于要保持真实正常音量的对数g, 我们不得不缩短进度条, 但当这个 setting_store.audioVolumeProcessing.volumeAmplify = -5 时, 这个进度条实际已经不存在了, 此时只能是正常音量的对数g。
+              > >   * 而当其进一步缩小的<-5时, 不但最小音量无法保持正常音量的对数g, 甚至滑块无法表示目前真实缩小的正常音量的对数值。(算成百分比时, 甚至出现了大于100%的离谱情况, 实际上这些只是更小的负的百分数)
+              > >  FIXME的完成小记: 放弃最小音量值, 始终要想保持正常音量的对数 g的方案。因为当 volumeAmplify 小于0 后, 这个方案是无意义的
+              > >                  * 此时, 我们最多只能保证其是 当前已被缩小音量<即此时的0对应的音量>的对数g。或者说此时可缩小的范围固定为g<因为我们的最大值固定为0这个数>。
+              > >                    * 即 当 volumeAmplify 小于0 后 的最新算法是 -5, 或者说-g
      -->
     <div
       :class="[
@@ -54,7 +66,7 @@
           :class="['w-[80%]']"
           v-model="setting_store.audioVolumeProcessing.volumeNormal"
           :max="0"
-          :min="-setting_store.audioVolumeProcessing.volumeAmplify - 5"
+          :min="-min"
           :step="0"
           label
           :label-value="labelValue"
@@ -72,13 +84,20 @@ import { computed, watch } from 'vue';
 
 const setting_store = useSettingStore();
 
+const min = computed(() => {
+  if (setting_store.audioVolumeProcessing.volumeAmplify > 0) {
+    return (
+      setting_store.audioVolumeProcessing.volumeAmplify + setting_store.audioVolumeProcessing.volumeNormalReduceScope
+    );
+  } else {
+    //  FIXME的完成小记: 对应步骤小记, 当setting_store.audioVolumeProcessing.volumeAmplify<0时, 我们使用固定的g值作为缩小幅度(或者说已被缩小的值<即0>为参考的对数g, 因为我们的最大值固定为0, 因此也是固定的g值)
+    //                   * g值 = setting_store.audioVolumeProcessing.volumeNormalReduceScope。
+    return setting_store.audioVolumeProcessing.volumeNormalReduceScope;
+  }
+});
+
 const labelValue = computed(() => {
-  const percentage = (
-    (1 - -setting_store.audioVolumeProcessing.volumeNormal / (setting_store.audioVolumeProcessing.volumeAmplify + 5)) *
-    100
-  )
-    .toFixed(2)
-    .split('.');
+  const percentage = ((1 - -setting_store.audioVolumeProcessing.volumeNormal / min.value) * 100).toFixed(2).split('.');
   return percentage[1] === '00' ? percentage[0] + '%' : percentage[0] + '.' + percentage[1] + '%';
 });
 
