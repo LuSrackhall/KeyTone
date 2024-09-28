f4990ee6b7635f2704a328054f0874dad4c0c4d8
LuSrackhall <3647637206@qq.com>
2024-09-29 01:24:42 +0800

    chore(sdk): 后端添加接收文件并本地保存的路径, 改为命令行参数的传入值; 接收传入值的全局变量, 已在上次提交时, 重构至正确的对应模块中。

--- sdk/server/server.go
+++ sdk/server/server.go
@@ -1,11 +1,13 @@
 package server
 
 import (
+	audioPackageConfig "KeyTone/audioPackage/config"
 	"KeyTone/config"
 	"KeyTone/logger"
 	"fmt"
 	"io"
 	"net/http"
+	"path/filepath"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -142,7 +144,7 @@ func keytonePkgRouters(r *gin.Engine) {
 		}
 
 		// 保存文件
-		err = ctx.SaveUploadedFile(file, "./"+file.Filename)
+		err = ctx.SaveUploadedFile(file, filepath.Join(audioPackageConfig.AudioPackagePath, file.Filename))
 		if err != nil {
 			ctx.JSON(http.StatusBadRequest, gin.H{
 				"message": "error: 文件添加失败, 后端保存过程中发生错误:" + err.Error(),
