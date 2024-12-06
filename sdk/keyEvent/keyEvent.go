3899839d90cff0a630e4e8859ee710365f6db4be
LuSrackhall <3647637206@qq.com>
2024-12-07 01:27:59 +0800

    chore(fix): 实现Dik与keyName实时映射的逻辑时, 发现此方案仅在单一按键按下时有效果。因为若涉及用户使用了按键映射工具, 多个 按键时会意外触发未知按键(已fix)和多余按键(前端键盘事件和sdk的键盘事件, 对多余按键的识别存在冲突, 具体冲突可见描述)。
    具体冲突:
    例如, 若用户系统中使用按键映射工具将'alt'+'j'映射到方向键'down'时。
    * 按住`alt`键(不抬起)的同时, 多次按下抬起`j`键。此时, sdk中会触发 'j' 和 'down' 对应的 Dik码, 而 前端 事件中 仅触发 'down', 而不会触发 'j'。

--- sdk/keyEvent/keyEvent.go
+++ sdk/keyEvent/keyEvent.go
@@ -42,40 +42,44 @@ func KeyEventListen() {
 	keycode_keycodeChan_map := make(map[uint16]chan hook.Event)
 
 	for ev := range evChan {
-		// if ev.Kind == 3 || ev.Kind == 4 || ev.Kind == 5 {
-		if ev.Kind == 4 || ev.Kind == 5 {
-			/* Kind
-			 *
-			 * KeyDown = 3  // 由于goHook的bug, KeyDown事件对象中, 无法判断实际的Keycode(即Keycode始终为0), 因此我们不使用这个事件。转而利用KeyHold事件代替此事件。
-			 * KeyHold = 4
-			 * KeyUp   = 5
-			 *
-			 * 鼠标的这些我们不需要,  只关注 button 即可
-			 * MouseUp    = 6
-			 * MouseHold  = 7
-			 * MouseDown  = 8
-			 * MouseMove  = 9
-			 * MouseDrag  = 10
-			 * MouseWheel = 11
-			 */
-			// if ev.Kind == 3 {
-			// 	println("down")
-			// 	println(ev.Keycode) // 按下时, 由于goHook的bug, 故无法判断实际的Keycode, 因此我们不使用这个事件。
-			// }
-			if _, exists := keycode_keycodeChan_map[ev.Keycode]; exists {
-				// logger.Debug("此时已经有了处理此按键发音的通道与其专用的goroutine, 因此无需进行任何创建操作, 只需要向其传递最新的事件信号即可")
-				keycode_keycodeChan_map[ev.Keycode] <- ev
-			} else {
-				// logger.Debug("此时还没有处理此按键发音的通道与其专用的goroutine, 因此需进行相关的创建操作, 并在创建后向其传递最新的事件信号")
-				// 创建此按键的专属通道channel
-				keycode_keycodeChan_map[ev.Keycode] = make(chan hook.Event)
-				// 创建此按键专属 按键事件处理 的 goroutine
-				go handleKeyEvent(keycode_keycodeChan_map[ev.Keycode])
-				// 将本次按键事件传递至相关通道channel
-				keycode_keycodeChan_map[ev.Keycode] <- ev
+		// 防止Dik为0的未知按键(前端一般称其为Unidentified)意外触发。
+		if ev.Keycode != 0 {
+			// if ev.Kind == 3 || ev.Kind == 4 || ev.Kind == 5 {
+			if ev.Kind == 4 || ev.Kind == 5 {
+				/* Kind
+				 *
+				 * KeyDown = 3  // 由于goHook的bug, KeyDown事件对象中, 无法判断实际的Keycode(即Keycode始终为0), 因此我们不使用这个事件。转而利用KeyHold事件代替此事件。
+				 * KeyHold = 4
+				 * KeyUp   = 5
+				 *
+				 * 鼠标的这些我们不需要,  只关注 button 即可
+				 * MouseUp    = 6
+				 * MouseHold  = 7
+				 * MouseDown  = 8
+				 * MouseMove  = 9
+				 * MouseDrag  = 10
+				 * MouseWheel = 11
+				 */
+				// if ev.Kind == 3 {
+				// 	println("down")
+				// 	println(ev.Keycode) // 按下时, 由于goHook的bug, 故无法判断实际的Keycode, 因此我们不使用这个事件。
+				// }
+				if _, exists := keycode_keycodeChan_map[ev.Keycode]; exists {
+					// logger.Debug("此时已经有了处理此按键发音的通道与其专用的goroutine, 因此无需进行任何创建操作, 只需要向其传递最新的事件信号即可")
+					keycode_keycodeChan_map[ev.Keycode] <- ev
+				} else {
+					// logger.Debug("此时还没有处理此按键发音的通道与其专用的goroutine, 因此需进行相关的创建操作, 并在创建后向其传递最新的事件信号")
+					// 创建此按键的专属通道channel
+					keycode_keycodeChan_map[ev.Keycode] = make(chan hook.Event)
+					// 创建此按键专属 按键事件处理 的 goroutine
+					go handleKeyEvent(keycode_keycodeChan_map[ev.Keycode])
+					// 将本次按键事件传递至相关通道channel
+					keycode_keycodeChan_map[ev.Keycode] <- ev
 
+				}
 			}
 		}
+
 	}
 }
 
