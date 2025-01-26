b7f8c6a0db86365ee103605a12fdc0193213b289
LuSrackhall <3647637206@qq.com>
2025-01-26 22:28:24 +0800

    fix(ui): 解决潜在的 过分监听端口变化引起的 资源消耗的问题。

--- frontend/src/boot/axios.ts
+++ frontend/src/boot/axios.ts
@@ -33,14 +33,32 @@ export const UpdateApi = (newPort: number) => {
 export default boot(({ app }) => {
   // 由于前端对于端口的变更时不确定的, 因此需要利用ipc持续监听端口变化，来更新api实例。(TIPS: 虽然本项目不涉及spa, 但后续有必要思考, spa中如何监听端口变化以做到前后端统一的应对。比如使用go启动某个spa时, 是否能做到向spa中传递一些参数这种事情。)
   if (process.env.MODE === 'electron') {
-    setInterval(async () => {
+    // TIPS: 由于即使端口变化, 也只会变化一次, 或是无需变化(99%的概率无需变化, 即使端口占用, sdk返回的端口也极大概率在渲染进程启动前返回)。
+    //       * 因此通过intervalId, 让其至多触发一次。(或是通过count, 让无需变化的情况下6s后自动停止, 避免持续监听造成的资源浪费)
+    let count = 0;
+    const intervalId = setInterval(async () => {
+      count++;
       const currentPort = window.myWindowAPI.getBackendPort(); // 这个逻辑放入boot的回调中恰到好处, 因为electron项目中我们无需在nodejs的主进程使用它。
       UpdateApi(currentPort);
       if (currentPort !== port) {
-        // 刷新重启整个应用
+        // 刷新重启整个应用(以刷新sse的端口)
         window.location.reload(); // 在此处可用
+        clearInterval(intervalId);
       }
+      if (count > 6) {
+        clearInterval(intervalId);
+      }
+      // console.log('count', count);
     }, 1000);
+    // ^^^^^^ TIPS: 上述是最终选择的方案, 下面介绍被放弃的方案
+    //  1. 在主进程中主动触发当前渲染进程中更新api实例的逻辑。
+    //       * electron的ipc提供了on/once的api, 可以监听对应事件并触发更新。(不过循环监听事件的方法不一定消耗比这个小)
+    //         * 可以利用on持续监听, 也可以利用once监听一次。(并且在electron的preload对应的on或once中, 也设置6s后自动停止, 避免持续监听造成的资源浪费)
+    //  2. 在主进程中, 控制生命周期, 已在开始启动渲染进程之前, 完成port的最终确认。
+    //     * 可以在 createWindow(); 调用之前, 确认端口好是否亦有sdk返回。
+    //     * 也可精确的 在createWindow内部逻辑的 mainWindow.loadURL(process.env.APP_URL); 或 mainWindow.loadFile('index.html'); 调用之前, 确认端口好是否亦有sdk返回。
+    //     * 如果未有sdk返回, 则不启动渲染进程, 并循环调用, 直到有sdk而端口数据返回。 端口数据返回, 则启动渲染进程, 并停止循环调用。
+    //     * 缺点: 需要精确控制生命周期, 且需要精确控制渲染进程的启动时机。(可能会影响窗口的启动速度)
   }
 
   // for use inside Vue files (Options API) through this.$axios and this.$api
