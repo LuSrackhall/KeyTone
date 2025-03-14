85189220ea4c4ea2f98f77666c58ef42a5523326
LuSrackhall <3647637206@qq.com>
2025-03-14 09:55:27 +0800

    chore(appx): 对于微软商店的版本, 不再向磁铁区域展示应用程序名称。

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -297,7 +297,7 @@ module.exports = configure(function (/* ctx */) {
           languages: ['zh-CN', 'en-US'], // 支持的语言
           // backgroundColor: 'transparent', // 设置为透明
           backgroundColor: '#ffffff', // 应用启动时的背景色
-          showNameOnTiles: true, // 在磁贴上显示应用名称
+          // showNameOnTiles: true, // 在磁贴上显示应用名称
           addAutoLaunchExtension: true, // 是否添加自动启动扩展
           // identityType: 'Store', // 指定应用程序的认证类型。'Store' 表示应用程序已经通过微软的认证，可以在Windows Store中发布。
           // identityCertificate: '../../KeyTone.pfx', // 指定应用程序的证书文件路径。这个文件通常用于对应用程序进行数字签名，以确保应用程序的完整性和安全性。
